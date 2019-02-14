c22d26125a53318493f411a1e7992fe528d32a04
Hunter <jenningsdhunter@gmail.com>
2019-02-13 22:38:59 -0500

    creating mobile overlay and functionality

--- src/js/modules/mobileOverlay.js
+++ src/js/modules/mobileOverlay.js
@@ -1,5 +1,6 @@
 import { Power3 } from 'gsap/EasePack';
 import { TweenLite } from 'gsap/TweenMax';
+import { TimelineLite } from 'gsap';
 
 /* global TweenLite TimelineLite */
 
@@ -9,55 +10,85 @@ export default class MobileOverlay {
     this.overlayBtn = document.querySelector('.mobile--navigation-button');
     this.mainWrapper = document.querySelector('.wrapper');
     this.overlayLinks = document.querySelectorAll('.mobile--navigation-overlay--container a');
+    this.isOverlayHidden = true;
     this.initEvents();
   }
 
   initEvents() {
     this.clickFn = () => {
-      this.animateOverlay();
-      this.animateOverlayBtn();
-      this.animateOverlayLinks();
+      // debugger;
+      if (this.isOverlayHidden) {
+        this.showOverlay();
+      } else {
+        this.hideOverlay();
+      }
     };
 
 
     this.overlayBtn.addEventListener('click', this.clickFn);
   }
 
+  showOverlay() {
+    this.toggleOverlay('show');
+  }
 
-  // TODO: Need to adjust timing and easing for this animation
 
-  animateOverlay() {
+  hideOverlay() {
+    this.toggleOverlay('hide');
+  }
+
+
+  toggleOverlay(action) {
+    // debugger;
     TweenLite.defaultEase = Power3.easeInOut;
+
 
     this.overlayTl = new TimelineLite();
 
-    this.overlayTl
-      .to(this.DOM.el, 0.4, {
-        opacity: 1,
-      })
-      .to(this.DOM.el, 0.4, {
-        display: 'flex',
-      }, '-=0.4')
-      .to(document.body, 0.4, {
-        overflow: 'hidden',
-      })
-      .to(this.mainWrapper, 0.4, {
-        overflow: 'hidden',
-      }, '-=0.4');
-  }
+    if (action === 'show') {
+      this.overlayTl
+        .set(this.overlayBtn, {
+          className: '+=close',
+        })
+        .to(this.DOM.el, 0.4, {
+          opacity: 1,
+        })
+        .to(this.DOM.el, 0.4, {
+          display: 'flex',
+        }, '-=0.4')
+        .staggerTo(this.overlayLinks, 0.2, {
+          opacity: 1,
+        }, 0.05)
+        .to(document.body, 0.4, {
+          overflow: 'hidden',
+        })
+        .to(this.mainWrapper, 0.4, {
+          overflow: 'hidden',
+        }, '-=0.4');
+      this.isOverlayHidden = false;
+    }
 
-  animateOverlayBtn() {
-    this.overlayBtn.classList.toggle('close');
-  }
+    if (action === 'hide') {
+      this.overlayTl
+        .set(this.overlayBtn, {
+          className: '-=close',
+        })
+        .staggerTo(this.overlayLinks, 0.2, {
+          opacity: 0,
+        }, 0.05)
+        .to(this.mainWrapper, 0.4, {
+          overflow: 'scroll',
+        }, '-=0.4')
+        .to(document.body, 0.4, {
+          overflow: '',
+        }, '-=0.4')
+        .to(this.DOM.el, 0.4, {
+          display: 'none',
+        }, '-=0.4');
 
-  animateOverlayLinks() {
-    TweenLite.defaultEase = Power3.easeInOut;
 
-    this.overlayLinksTl = new TimelineLite();
-
-    this.overlayLinksTl.staggerTo(this.overlayLinks, 0.3, {
-      opacity: 1,
-      y: -100,
-    }, 0.05);
+      this.isOverlayHidden = true;
+      // this.overlayBtn.classList.remove('close');
+    }
   }
 }
